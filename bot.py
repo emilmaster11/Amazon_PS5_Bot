3a64476ce584d5ee50b69b8981b4b260e6a117cd
Emil <39263062+emilmaster11@users.noreply.github.com>
2022-09-19 14:21:39 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,85 @@
+import time
+import random
+from random import randint 
+from selenium import webdriver
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class AmazonBot():
+
+    def __init__(self,profile,directory,user_agent):
+        options=webdriver.ChromeOptions()
+        self.options = options
+
+        self.profile = profile
+        options.add_argument("--user-data-dir=C:\\Users\\emilb\\Desktop\\Python_projekte\\BOTS\\Chrome_Profile\\"+str(profile))
+        
+        self.directory = directory
+        options.add_argument("--profile-directory="+str(directory))
+
+
+        options.add_argument("user-agent="+user_agent)    
+   
+    
+        driver  = uc.Chrome(options=options)
+        self.driver = driver
+    
+
+    def go_to_product(self,link):
+        driver = self.driver
+
+        driver.get(link)
+
+
+    def put_to_shopping_card(self):
+        driver = self.driver
+    
+ 
+        shopping_cart_button = driver.find_element(By.XPATH, '//*[@id="submit.add-to-cart"]//*')
+        shopping_cart_button.click()
+
+    
+        try:
+            insurance_button = WebDriverWait(driver,5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="attachSiNoCoverage"]/span/input')))
+            insurance_button.click()
+            
+            go_to_checkout_button = WebDriverWait(driver,5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="attach-sidesheet-checkout-button"]/span/input')))
+            go_to_checkout_button.click()
+            return
+
+        except:pass
+     
+        go_to_checkout_button2 = WebDriverWait(driver,5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="sc-buy-box-ptc-button"]/span/input'))) 
+        go_to_checkout_button2.click()
+
+
+    def buy_now(self):
+        driver = self.driver
+
+        buy_now_button = driver.find_element(By.XPATH,'//*[@id="placeYourOrder"]/span/input')
+        buy_now_button.click()
+        
+        time.wait(100)
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,10 @@
+from bot import AmazonBot
+
+
+if __name__ == "__main__":
+    ps5 = AmazonBot("GoogleChromeProfile2","Profile2","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36")
+
+    ps5.go_to_product(r"https://www.amazon.de/ECHTPower-Controller-Ladestation-Schnelllade-Playstation/dp/B08PCMKR2C/?_encoding=UTF8&pd_rd_w=20NbL&content-id=amzn1.sym.19b3c00c-468f-440c-b90d-756778c282ea&pf_rd_p=19b3c00c-468f-440c-b90d-756778c282ea&pf_rd_r=HSWNHR6Q78DTABHR2FK7&pd_rd_wg=bM3Q5&pd_rd_r=f437264f-8e62-4852-a720-751639a9b72d&ref_=pd_gw_ci_mcx_mi")
+    ps5.put_to_shopping_card()
+    
+    ps5.buy_now()
